0c36d368c853b12ef388371d550b00c01b41e64f
aniketcodes <aniketsinha1304@gmail.com>
2022-10-14 00:52:21 +0530

    Added function to get transactions from etherscan

---
+++ helpers/external.js
@@ -0,0 +1,20 @@
+const Axios = require("axios");
+
+module.exports = {
+
+  /**
+   * @description It fetches transactions for an address from etherscan api
+   * @param  address It is the address for which transactions are to be fetched
+   * @returns An object transaction which is an array of transactions
+   */
+  fetchNormalTransactions: async function (address) {
+    try {
+      const { data } = await Axios.get(
+        `${process.env.ETHERSCAN_API_URL}&address=${address}&apiKey=${process.env.ETHERSCAN_API_KEY}`
+      );
+      return { transactions: data?.result };
+    } catch (error) {
+      throw error;
+    }
+  },
+};
